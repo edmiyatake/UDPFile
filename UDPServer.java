d64a05b1ec781b8549d21109fc5ff38a398f4d41
Edwin Miyatake <edmiyatake@gmail.com>
2024-11-19 12:51:32 -0800

    now have a working connection

--- UDPServer.java
+++ UDPServer.java
@@ -9,16 +9,18 @@ public class UDPServer {
             while (true) { 
                 DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
                 socket.receive(receivePacket);
+
                 String sentence = new String(receivePacket.getData());
-                System.out.println("Received: " + sentence);
-                String stringData = "Hello client";
+                System.out.println("Received: " + sentence); // just making sure data is received
+
+                String stringData = "Hello client"; // message that is getting sent
                 sendData = stringData.getBytes();
+
                 InetAddress clientIpAddress = receivePacket.getAddress();
                 int clientPort = receivePacket.getPort();
                 DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, clientIpAddress, clientPort);
                 socket.send(sendPacket);
-                
-
+                socket.close();
             }
         } 
         catch (Exception e) {
